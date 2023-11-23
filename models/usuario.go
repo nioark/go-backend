95f0f39402712f0999ff4e8bd7f28fa378ee103f
nio <nicolasrausch.c@gmail.com>
2023-11-23 07:40:07 -0300

    first commit graphql

--- models/usuario.go
+++ models/usuario.go
@@ -1,7 +1,23 @@
 package models
 
+import (
+	"github.com/graphql-go/graphql"
+)
+
 type Usuario struct {
 	Id       uint64 `json:"id"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
 }
+
+var userType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "usuario",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.Int,
+		},
+		"name": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
